496be7cef6ea25ad6607da6decc514746f53d085
hsuanchia <wxes95301@gmail.com>
2020-01-05 16:59:30 +0800

    r/l shifter

--- snake/snake.v
+++ snake/snake.v
@@ -6,19 +6,21 @@ module snake(
 	
 	reg [7:0] status [7:0];
 	reg long = 3;
+	reg [1:0] moveway;
 	
 	initial
 		begin
+			moveway = 2'b11; //left = 00,down = 01,up = 10,right = 11
 			data_r = 8'b11111111;
 			data_g = 8'b11111111;
 			data_b = 8'b11111111;
-			status[0] = 8'b11111111;
-			status[1] = 8'b01111111;
-			status[2] = 8'b11111111;
-			status[3] = 8'b11111111;
-			status[4] = 8'b11111111;
-			status[5] = 8'b11111111;
-			status[6] = 8'b11111111;
+			status[0] = 8'b00000000;
+			status[1] = 8'b00000000;
+			status[2] = 8'b00000000;
+			status[3] = 8'b00000000;
+			status[4] = 8'b00000000;
+			status[5] = 8'b00000000;
+			status[6] = 8'b00000000;
 			status[7] = 8'b11111111;
 		end
 			
@@ -27,57 +29,66 @@ module snake(
 	divfreq F0(clk,clk_div);
 	divfreq_mv F1(clk, clk_mv);
 	
-	always@(posedge clk_div)
+	always@(posedge clk_div) // Frame per second refresh
 		begin
 			if (cnt >= 7)
 				cnt = 0;
 			else
 				cnt = cnt + 1;
 			comm = {1'b1,cnt};
-			
+ 			
 			data_g = status[cnt];
 			
 		end
-	
-	always@(posedge clk_mv, posedge clear)
+		
+//		if(appely == cnt)
+//		{
+//				int applex = 5;
+//				reg tmp = 8'b00000000;
+//				rightshift
+//				{
+//					
+//				}
+//				
+//		}
+//	
+	always@(posedge clk_mv,posedge clear)
 		begin
+		bit [7:0] tmp;
+		integer index = 0;
 			if(clear)
+				begin
+					status[0] = 8'b01111111;
+					status[1] = 8'b01111111;
+					status[2] = 8'b11111111;
+					status[3] = 8'b11111111;
+					status[4] = 8'b11111111;
+					status[5] = 8'b11111111;
+					status[6] = 8'b11111111;
+					status[7] = 8'b11111111;
+				end
+			else
 			begin
-				status[0] = 8'b00001111;
-				status[1] = 8'b11111111;
-				status[2] = 8'b11111111;
-				status[3] = 8'b11111111;
-				status[4] = 8'b11111111;
-				status[5] = 8'b11111111;
-				status[6] = 8'b11111111;
-				status[7] = 8'b11111111;
-			end
-			else if(direction[3])
-				begin
-					if (count >= 7)
-						count = 0;
-					else
-						begin
-							count = count + 1;
-							status[count-1] = 8'b11111111;
-							status[count] = 8'b00001111;
-						
-						end
-
-				end
-			else if(direction[0])
-				begin
-					if (count <= 0)
-						count = 7;
-					else
-						begin
-							count = count - 1;
-							status[count] = 8'b00001111;
-							status[count+1] = 8'b11111111;
-						end
-
-				end
+				if(direction[3])
+					begin
+						tmp = status[7];
+						for(index = 7;index >= 1;index--)
+								status[index] = status[index-1];
+						status[0] = tmp;
+					end
+				else if(direction[0])
+					begin
+						tmp = status[0];
+						for(index = 1;index <= 7;index++)
+								status[index-1] = status[index];
+						status[7] = tmp;
+					end
+			end		
+		
 		end
+	
+		
+		
 endmodule
 
 
@@ -99,7 +110,7 @@ module divfreq_mv(input clk, output reg clk_mv);
 	reg[35:0] count;
 	always@(posedge clk)
 		begin
-			if(count > 35000)
+			if(count > 3500000)
 				begin
 					count <= 35'b0;
 					clk_mv <= ~clk_mv;
